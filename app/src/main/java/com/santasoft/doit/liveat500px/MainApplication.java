7b94ebdcf0933fea025410f84693fbc6cf1485bd
Natta Wang <xenogew@gmail.com>
2016-06-22 00:25:12 +0700

    construct the initialize project to-do

--- app/src/main/java/com/santasoft/doit/liveat500px/MainApplication.java
+++ app/src/main/java/com/santasoft/doit/liveat500px/MainApplication.java
@@ -2,6 +2,8 @@ package com.santasoft.doit.liveat500px;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by Natta Wang on 21/6/2016.
  */
@@ -11,14 +13,11 @@ public class MainApplication extends Application {
         super.onCreate();
 
         // Initialize things here
+        Contextor.getInstance().init(getApplicationContext());
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
-    }
-
-    private void initialState() {
-
     }
 }
--- app/src/main/java/com/santasoft/doit/liveat500px/activity/MainActivity.java
+++ app/src/main/java/com/santasoft/doit/liveat500px/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.santasoft.doit.liveat500px.R;
+import com.santasoft.doit.liveat500px.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, MainFragment.newInstance())
+                    .commit();
+        }
     }
 }
