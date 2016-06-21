4746823336738e79bcd40c4667e609e265517b49
Natta Wang <xenogew@gmail.com>
2016-06-22 00:03:29 +0700

    first commit

---
+++ app/src/main/java/com/santasoft/doit/liveat500px/MainApplication.java
@@ -0,0 +1,24 @@
+package com.santasoft.doit.liveat500px;
+
+import android.app.Application;
+
+/**
+ * Created by Natta Wang on 21/6/2016.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize things here
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    private void initialState() {
+
+    }
+}
---
+++ app/src/main/java/com/santasoft/doit/liveat500px/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.santasoft.doit.liveat500px.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.santasoft.doit.liveat500px.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
